a2a2d1bf0f63090d0186c6d6e4d65e1548fb3e85
Herbert <421271727@qq.com>
2017-03-02 00:23:49 +0800

    Interface
    Front End Interface

---
+++ Server/Graduation/Controllers/UserController/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Graduation.Model;
+using Graduation.BusinessLogic.UserManager;
+
+namespace Graduation.Controllers.UserController
+{
+    public class UserController: Controller
+    {
+        [HttpPost]
+        public JsonResult Login(Users user)
+        {
+            int msg = 0;
+            UserLogin login = new UserLogin(user);
+            msg=login.DoLogin();
+            return Json(msg);
+        }
+
+        public JsonResult Register(Users user)
+        {
+            int msg = 0;
+            UserRegister register = new UserRegister(user);
+            msg=register.DoRegister();
+            return Json(msg);
+        }
+    }
+}
---
+++ Server/Graduation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Graduation.Startup))]
+namespace Graduation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
