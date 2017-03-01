7fc77f48903a9a7d93befb5c0509c646992295ae
Herbert <421271727@qq.com>
2017-03-01 23:51:50 +0800

    Register and Login
    Register and Login

--- Server/Graduation.BusinessLogic/UserManager/Login.cs
+++ Server/Graduation.BusinessLogic/UserManager/Login.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Graduation.Model;
-using Graduation.Data;
+using Graduation.Data.Context;
 
 namespace Graduation.BusinessLogic.UserManager
 {
-    public class Login
+    public class UserLogin
     {
-        public Login(Users user)
+        private UsersDBContext _userDb = new UsersDBContext();
+        private Users _user = new Users();
+        public UserLogin(Users user)
         {
+            this._user = user;
+        }
 
+        public int DoLogin()
+        {
+            Users findUser = _userDb.Users.FirstOrDefault<Users>(a => a.UserName == _user.UserName && a.PassWord == _user.PassWord);
+            if (findUser != null)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
