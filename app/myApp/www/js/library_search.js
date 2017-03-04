6db2717ecd1ec9fd1c818c58fa4c50c78c6509c1
foreverjiangting <1259920090@qq.com>
2017-03-04 15:07:40 +0800

    增加馆藏查询模块的页面

--- app/myApp/www/js/library_search.js
+++ app/myApp/www/js/library_search.js
@@ -3,9 +3,23 @@
 angular.module('starter.search', [])
 
 .controller('library_searchCtrl',['$scope','$ionicPopup', '$timeout', '$stateParams','$http','$ionicScrollDelegate','$location','$window' ,'$ionicHistory',function($scope, $ionicPopup, $timeout,$stateParams,$http,$ionicScrollDelegate,$location,$window,$ionicHistory){
+    //console.log('33333');
+    $scope.search = function(){
+    	console.log('jtt');
+    };
+
+    //请求数据
+    $http({  
+            url : "../data/library_search.json",  
+            method : "get"         
+           // headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' }
+
+        }).success(function(data) { 
+            $scope.data =  data;
+        
+        })
+
     
-    console.log('jt');
-  
 }])
 
 
