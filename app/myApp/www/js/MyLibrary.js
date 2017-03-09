35a06a82e471f60eebcf5b7838eec7381ab2f282
foreverjiangting <1259920090@qq.com>
2017-03-09 15:05:17 +0800

    修改忽略文件bug

---
+++ app/myApp/www/js/MyLibrary.js
@@ -0,0 +1,9 @@
+//我的图书馆
+
+angular.module('MyLibrary', [])
+
+.controller('MyLibraryCtrl',['$scope','$ionicPopup', '$timeout', '$stateParams','$http','$ionicScrollDelegate','$location','$window' ,'$ionicHistory',function($scope, $ionicPopup, $timeout,$stateParams,$http,$ionicScrollDelegate,$location,$window,$ionicHistory){
+    
+    console.log('jt');
+  
+}])
