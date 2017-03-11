444a3859372a98e924e3e0bff350adeaabd6b0f6
foreverjiangting <1259920090@qq.com>
2017-03-11 22:24:01 +0800

    添加数字资源模块

--- app/myApp/www/js/shejiao.js
+++ app/myApp/www/js/shejiao.js
@@ -4,6 +4,27 @@ angular.module('shejiao', [])
 
 .controller('shejiaoCtrl',['$scope','$ionicPopup', '$timeout', '$stateParams','$http','$ionicScrollDelegate','$location','$window' ,'$ionicHistory',function($scope, $ionicPopup, $timeout,$stateParams,$http,$ionicScrollDelegate,$location,$window,$ionicHistory){
     
-    console.log('jt');
+    $scope.wopList = [
+        {
+        	"bookName":"JavaScipt高级程序设计",
+        	"position":"2F14排第三本",
+        	"date":"2017-03-11"
+        },
+         {
+        	"bookName":"css的那些事",
+        	"position":"2F14排第四本",
+        	"date":"2017-03-11"
+        },
+         {
+        	"bookName":"JavaScipt高级程序设计",
+        	"position":"2F14排第三本",
+        	"date":"2017-03-11"
+        },
+         {
+        	"bookName":"JavaScipt高级程序设计",
+        	"position":"2F14排第三本",
+        	"date":"2017-03-11"
+        }
+    ];
   
 }])
