be7d92abe09f4d5718bccdbf3c354e6dfa2113fd
foreverjiangting <1259920090@qq.com>
2017-03-11 11:05:27 +0800

    新书推荐

--- app/myApp/www/js/newBookRecommand.js
+++ app/myApp/www/js/newBookRecommand.js
@@ -4,6 +4,43 @@ angular.module('newBookRecommand', [])
 
 .controller('newBookRecommandCtrl',['$scope','$ionicPopup', '$timeout', '$stateParams','$http','$ionicScrollDelegate','$location','$window' ,'$ionicHistory',function($scope, $ionicPopup, $timeout,$stateParams,$http,$ionicScrollDelegate,$location,$window,$ionicHistory){
     
-    console.log('jt');
+     //解决web浏览器只跟踪一个历史浏览记录
+    $ionicHistory.nextViewOptions({
+         disableBack: true
+    });  
+    
+    $scope.recommandBookList = [
+      {
+      	"img":"../img/ajaxwqsc.jpg",
+      	"bookName":"javaScript高级教程",
+      	"bookAuthor":"李松峰",
+      	"chubanshe":"上海电子科技出版社"
+      },{
+      	"img":"../img/angularjskfxyd.jpg",
+      	"bookName":"javaScript高级教程",
+      	"bookAuthor":"李松峰",
+      	"chubanshe":"上海电子科技出版社"
+      },{
+      	"img":"../img/bxgzldm.jpg",
+      	"bookName":"javaScript高级教程",
+      	"bookAuthor":"李松峰",
+      	"chubanshe":"上海电子科技出版社"
+      },{
+      	"img":"../img/ajaxwqsc.jpg",
+      	"bookName":"javaScript高级教程",
+      	"bookAuthor":"李松峰",
+      	"chubanshe":"上海电子科技出版社"
+      },{
+      	"img":"../img/bxgzldm.jpg",
+      	"bookName":"javaScript高级教程",
+      	"bookAuthor":"李松峰",
+      	"chubanshe":"上海电子科技出版社"
+      },{
+      	"img":"../img/angularjskfxyd.jpg",
+      	"bookName":"javaScript高级教程",
+      	"bookAuthor":"李松峰",
+      	"chubanshe":"上海电子科技出版社"
+      }
+    ];
   
 }])
