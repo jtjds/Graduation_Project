729aeddc0f74cec3f81a5e644ece0126df22bfd1
foreverjiangting <1259920090@qq.com>
2017-03-11 13:24:11 +0800

    消息通知

--- app/myApp/www/js/messageForm.js
+++ app/myApp/www/js/messageForm.js
@@ -5,7 +5,38 @@ angular.module('messageForm', [])
 
 .controller('messageFormCtrl',['$scope','$ionicPopup', '$timeout', '$stateParams','$http','$ionicScrollDelegate','$location','$window' ,'$ionicHistory',function($scope, $ionicPopup, $timeout,$stateParams,$http,$ionicScrollDelegate,$location,$window,$ionicHistory){
     
-    console.log('jt');
+   $scope.messageList = [
+        {
+	    	"message":"婷风",
+	    	"text":"您好 ，有新书上架哦！",
+	    	"isChecked":true,
+	    	"date":"2017-03-12"
+        },
+        {
+	    	"message":"婷风",
+	    	"text":"您好 ，有新书上架哦！",
+	    	"isChecked":true,
+	    	"date":"2017-03-11"
+        },
+         {
+	    	"message":"婷风",
+	    	"text":"您好 ，有新书上架哦！",
+	    	"isChecked":false,
+	    	"date":"2017-03-08"
+        },
+         {
+	    	"message":"婷风",
+	    	"text":"您好 ，距离您还书的日期还剩下2天，请尽快查阅哦！",
+	    	"isChecked":false,
+	    	"date":"2017-02-15"
+	    },
+	    {
+	    	"message":"婷风",
+	    	"text":"您好 ，距离您还书的日期还剩下3天，请尽快查阅哦！",
+	    	"isChecked":true,
+	    	"date":"2017-02-14"
+	    }
+   ];
   
 }])
 
